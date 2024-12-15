57e15621a77dce896f367bf1e1b4c388fa70404d
Krishna-Aggarwal-11 <krishnaaggarwal11may@gmail.com>
2024-12-15 19:22:39 +0530

    database setup (convex) added

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,8 +1,14 @@
+"use client"
+
+import { useQuery } from "convex/react"
+
 import { Navbar } from "./navbar"
 import { TemplateGallery } from "./template-gallery"
+import { api } from "../../../convex/_generated/api"
 
 
 const HomePage = () => {
+  const documents = useQuery(api.documents.get)
   return (
     <div className="min-h-screen flex flex-col">
       <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
@@ -10,6 +16,9 @@ const HomePage = () => {
       </div>
       <div className="mt-16">
         <TemplateGallery/>
+        {documents?.map((document) => (
+          <div key={document._id}>{document.title}</div>
+        ))}
       </div>
     </div>
   )
