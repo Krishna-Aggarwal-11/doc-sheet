df254e036085ed935e0a357d0df129858f7b9dc4
Krishna-Aggarwal-11 <krishnaaggarwal11may@gmail.com>
2024-12-16 01:00:56 +0530

    create and list document

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,14 +1,16 @@
 "use client"
 
-import { useQuery } from "convex/react"
+import { usePaginatedQuery } from "convex/react"
 
 import { Navbar } from "./navbar"
+import {DocumentTable} from "./document-table"
 import { TemplateGallery } from "./template-gallery"
+
 import { api } from "../../../convex/_generated/api"
 
 
 const HomePage = () => {
-  const documents = useQuery(api.documents.get)
+  const {results , status , loadMore} = usePaginatedQuery(api.documents.get,{},{initialNumItems: 10})
   return (
     <div className="min-h-screen flex flex-col">
       <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
@@ -16,9 +18,11 @@ const HomePage = () => {
       </div>
       <div className="mt-16">
         <TemplateGallery/>
-        {documents?.map((document) => (
-          <div key={document._id}>{document.title}</div>
-        ))}
+        <DocumentTable 
+          documents={results}
+          status={status}
+          loadMore={loadMore}
+        />
       </div>
     </div>
   )
--- src/app/(home)/template-gallery.tsx
+++ src/app/(home)/template-gallery.tsx
@@ -11,10 +11,27 @@ import {
 import { templates } from "@/constants/templates";
 
 import { cn } from "@/lib/utils";
-
+import { useMutation } from "convex/react";
+import { useRouter } from "next/navigation";
+import { api } from "../../../convex/_generated/api";
+import { useState } from "react";
 
 export const TemplateGallery = () => {
-  const isCreating = false;
+  const router = useRouter();
+  const create = useMutation(api.documents.create);
+  const [isCreating, setIsCreating] = useState(false);
+
+  const onTemplateClick = async (title: string, initialContent: string) => {
+    setIsCreating(true);
+    create({ title, initialContent })
+    .then((documnetId) => {
+      router.push(`/documents/${documnetId}`);
+    }).finally(() => {
+      setIsCreating(false);
+    });
+
+  };
+
   return (
     <div className="bg-[#F1F3F4]">
       <div className="max-w-screen-xl mx-auto px-16 py-6 flex flex-col gap-y-4">
@@ -34,7 +51,7 @@ export const TemplateGallery = () => {
                 >
                   <button
                     disabled={isCreating}
-                    onClick={() => {}}
+                    onClick={() => onTemplateClick(template.label, "")}
                     style={{
                       backgroundImage: `url(${template.imageUrl})`,
                       backgroundSize: "cover",
@@ -42,15 +59,16 @@ export const TemplateGallery = () => {
                       backgroundRepeat: "no-repeat",
                     }}
                     className="size-full hover:bg-blue-50 hover:border-blue-500 rounded-sm border transition flex flex-col items-center justify-center gap-y-4 bg-white"
-                  >
-                  </button>
-                    <p className="text-sm font-medium truncate">{template.label}</p>
+                  ></button>
+                  <p className="text-sm font-medium truncate">
+                    {template.label}
+                  </p>
                 </div>
               </CarouselItem>
             ))}
           </CarouselContent>
-          <CarouselPrevious/>
-          <CarouselNext/>
+          <CarouselPrevious />
+          <CarouselNext />
         </Carousel>
       </div>
     </div>
